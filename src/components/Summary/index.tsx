6cf40fd551f2c59646400839a9a83221939f129a
Erica Calaça <ericazpc@gmail.com>
2025-09-12 14:01:22 -0700

    Created the component Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,32 @@
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
+import { SummaryCard, SummaryContainer } from "./styles";
+
+export function Summary(){
+    return(
+        <SummaryContainer>
+            <SummaryCard>
+                <header>
+                    <span>Income</span>
+                    <ArrowCircleUp size={32} color="#00b37e"/>
+                </header>
+                <strong>$ 2345.43</strong>
+            </SummaryCard>
+
+            <SummaryCard>
+                <header>
+                    <span>Expense</span>
+                    <ArrowCircleDown size={32} color="#f75a68"/>
+                </header>
+                <strong>$ 2345.43</strong>
+            </SummaryCard>
+
+            <SummaryCard variant="green">
+                <header>
+                    <span>Balance</span>
+                    <CurrencyDollar size={32} color="#fff"/>
+                </header>
+                <strong>$ 2345.43</strong>
+            </SummaryCard>
+        </SummaryContainer>
+    )
+}
